ed1f7da78d336f34617491a52269178a1d6bf223
AksaMJML <akaks0186@gmail.com>
2025-01-08 16:22:30 +0530

    install and imported jsonwebtoken 🔗

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,24 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"; 
 
 export function registerUser(req,res){
    
     const data = req.body;
-
     data.password = bcrypt.hashSync(data.password,10)
-
     const newUser = new User(data);
-
     newUser.save().then(()=>
     {
         res.json ({
             message : "user added successfully"
         })
     }).catch((error)=>{
-            res.status(500).json({error : "user registration failed"})
-        
+            res.status(500).json({error : "user registration failed"})     
     }
     )
 };
 
 export function loginUser(req,res){
-
     const data = req.body;
 
     User.findOne({
@@ -38,6 +34,7 @@ export function loginUser(req,res){
 
                 if(isPasswordCorrect)
                 {
+                    const token = jwt.sign({})
                     res.json({
                         message : "login successfull"
                     })
