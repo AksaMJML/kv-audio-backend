c88048ca0f4e69c72ccdf62f50b78bb7efcb2fc8
AksaMJML <akaks0186@gmail.com>
2025-01-30 22:36:41 +0530

    addInquiry function created 🪪

--- controllers/inquiryController.js
+++ controllers/inquiryController.js
@@ -1,1 +1,21 @@
 import Inquiry from "../models/inquiry.js";
+import Review from "../models/review.js";
+
+export async function  addInquiry(req,res) {
+    if(req.user == null){
+        res.status(401).json({
+            message : "please login to the system and try again"
+        })
+        return;
+    }
+    const data = req.body;
+
+    data.email = req.user.email;
+    try{
+        const newInquiry = new Inquiry(data);
+        await Inquiry.save();
+    }catch(error){
+        console.error("Error adding inquiry:", error);
+        res.status(500).json({ message: "Inquiry addition failed", error: error.message });
+    }
+}
